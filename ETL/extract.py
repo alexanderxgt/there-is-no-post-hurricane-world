44f473ac57ab89be88e8eab9df2d0b8c95e17fd9
alexanderxgt <alexanderxgt@gmail.com>
2024-11-26 17:15:54 -0800

    ETL Workflow: Transform_Disaster operational

--- ETL/extract.py
+++ ETL/extract.py
@@ -6,54 +6,52 @@ def extract_country_data(country_code, save_to_csv=False):
     Extracts data for a specific country from both development and disaster datasets.
 
     Args:
-        country_code (str): The ISO code of the country to extract (e.g., "USA").
+        1. country_code (str): The ISO code of the country to extract (e.g., "USA").
+        2. save_to_csv (bool): If True, saves the transformed DataFrames as CSV files.
 
     Returns:
-        1. Two DataFrames for a specified country: the first with development data, the second with disaster data.
-        2. (Optional) Two CSV files with the data.
+        1. Two DataFrames for a specified country: development_data and disaster_data.
+        2. (Optional) Two CSV files with the data (e.g., USA_disaster_data_.csv and USA_development_data.csv).
     """
     try:
-        # Development data extraction
+        # Development Data Extraction
         development_file_path = "../data/raw/development_data_raw.csv"
         if not os.path.exists(development_file_path):
-            raise FileNotFoundError(f"WDI file not found at {development_file_path}")
+            raise FileNotFoundError(f"[EXTRACT ERROR] WDI file not found at {development_file_path}")
 
         development_data = pd.read_csv(development_file_path)
         development_data = development_data[development_data['Country Code'] == country_code]
 
         if development_data.empty:
-            print(f"Error finding WDI data for Country Code: {country_code}")
+            print(f"[EXTRACT ERROR] Error finding WDI data for Country Code: {country_code}")
         else:
-            print(f"Successfully extracted WDI data for {country_code}.")
+            print(f"[EXTRACT INFO] Extracting WDI data for {country_code}...")
 
-        # Disaster data extraction
+        # Disaster Data Extraction
         disaster_file_path = "../data/raw/disaster_data_raw.csv"
         if not os.path.exists(disaster_file_path):
-            raise FileNotFoundError(f"EM-DAT file not found at {disaster_file_path}")
+            raise FileNotFoundError(f"[EXTRACT ERROR] EM-DAT file not found at {disaster_file_path}")
 
         disaster_data = pd.read_csv(disaster_file_path)
         disaster_data = disaster_data[disaster_data['ISO'] == country_code]
 
         if disaster_data.empty:
-            print(f"Error finding EM-DAT data for ISO: {country_code}")
+            print(f"[EXTRACT ERROR] Error finding EM-DAT data for ISO: {country_code}")
         else:
-            print(f"Successfully extracted EM-DAT data for {country_code}.")
+            print(f"[EXTRACT INFO] Extracting EM-DAT data for {country_code}...")
 
         # Save data to CSV if requested
         if save_to_csv:
-            os.makedirs("../data/unfiltered_ETLoutputs", exist_ok=True)
-            development_data.to_csv(f"../data/unfiltered_ETLoutputs/{country_code}_development_data.csv", index=False)
-            disaster_data.to_csv(f"../data/unfiltered_ETLoutputs/{country_code}_disaster_data.csv", index=False)
-            print(f"Two CSVs created for {country_code} at ../data/outputs: PHL_development_data and PHL_disaster_data.")
+            os.makedirs("../data/unfiltered_ETL_outputs", exist_ok=True)
+            development_data.to_csv(f"../data/unfiltered_ETL_outputs/{country_code}_development_data.csv", index=False)
+            disaster_data.to_csv(f"../data/unfiltered_ETL_outputs/{country_code}_disaster_data.csv", index=False)
+            print(f"[EXTRACT INFO] Creating two CSVs for {country_code} at ../data/_unfiltered_ETL_outputs: {country_code}_development_data and {country_code}_disaster_data...")
 
 
-        print(f"Two DataFrames created for {country_code}: development_data and disaster_data.")
+        print(f"[EXTRACT INFO] Creating two DataFrames for {country_code}: development_data and disaster_data...")
         return development_data, disaster_data
 
     except Exception as e:
-        print(f"Error executing extract_country_data for {country_code}: {e}")
+        print(f"[EXTRACT ERROR] Error executing extract_country_data for {country_code}: {e}")
         raise
 
-## TEST ##
-if __name__ == "__main__":
-    extract_country_data(country_code="PHL", save_to_csv=True)
--- ETL/transform.py
+++ ETL/transform.py
@@ -1,14 +1,43 @@
-def transform_data(development_data, disaster_data, save_to_csv=False):
+import os
+
+
+def transform_disaster_data(
+        # all default figures chosen during Exploratory Data Analysis
+        country_code,
+        disaster_data,
+        df_start_year=2008,
+        df_end_year=2020,
+        min_total_affected=2000000,
+        save_to_csv=False
+):
     """
-    Transforms development and disaster DataFrames for a specific country based
-    on parameters decided during Exploratory Data Analysis phase of this project.
+    Transforms disaster data for a specific country based on user args.
 
     Args:
-        development_data (pd.DataFrame): Raw development data.
-        disaster_data (pd.DataFrame): Raw disaster data.
+       1. country_code (str): ISO code of the country.
+       2. disaster_data (pd.DataFrame): Raw disaster data.
+       3. df_start_year (int): Start year for filtering (default is 2008).
+       4. df_end_year (int): End year for filtering (default is 2020).
+       5. min_total_affected (int): Minimum total affected for filtering (default is 2,000,000).
+       6. save_to_csv (bool): If True, saves the transformed DataFrame to a CSV.
 
     Returns:
-        1. Two filtered DataFrames for a specified country: transformed_development_data, transformed_disaster_data.
-        2. (Optional) Two CSV files with the transformed data.
+        1. A filtered DataFrame for a specified country: disaster_data_transformed.
+        2. (Optional) A CSV file with the transformed data (e.g., USA_disaster_data_transformed.csv).
 
     """
+    disaster_data_transformed = disaster_data[(disaster_data['Start Year'] >= df_start_year) & (disaster_data['Start Year'] <= df_end_year)]
+    print(f"[TRANSFORM INFO] Filtering {country_code} EM-DAT data to only include disasters between {df_start_year} and {df_end_year}...")
+
+    disaster_data_transformed = disaster_data_transformed[disaster_data_transformed['Total Affected'] >= min_total_affected]
+    print(f"[TRANSFORM INFO] Filtering {country_code} EM-DAT data to only include disasters with {min_total_affected} or more affected...")
+
+    # Save data to CSV if requested
+    if save_to_csv:
+        os.makedirs("../data/filtered_ETL_outputs", exist_ok=True)
+        disaster_data_transformed.to_csv(f"../data/filtered_ETL_outputs/{country_code}_disaster_data_transformed.csv", index=False)
+        print(f"[TRANSFORM INFO] Creating CSV for disaster data at ../data/filtered_ETL_outputs/{country_code}_disaster_data_transformed.csv...")
+
+    return disaster_data_transformed
+
+
