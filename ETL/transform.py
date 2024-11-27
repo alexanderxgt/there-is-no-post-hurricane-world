0516f25ecff28bb5fa68a134c927a79b9810a2fb
alexanderxgt <alexanderxgt@gmail.com>
2024-11-27 00:15:58 -0800

    ETL Workflow: transform.py is operational

--- ETL/transform.py
+++ ETL/transform.py
@@ -5,8 +5,8 @@ def transform_development_data(
         # all default figures chosen during Exploratory Data Analysis
         country_code,
         development_data,
-        df_min_year=2008,
-        df_max_year=2020,
+        df_min_year=2007,
+        df_max_year=2021,
         save_to_csv=False,
 ):
     """
@@ -26,43 +26,63 @@ def transform_development_data(
         2. (Optional) A CSV file with the transformed data (e.g., USA_development_data_transformed.csv).
 
     """
-    development_data_transformed = development_data.copy()
+    try:
+        development_data_transformed = development_data.copy()
 
-    development_data_transformed = development_data_transformed.loc[:,
-                                   ['Country Code', 'Indicator Name'] +
-                                   [str(year) for year in range(df_min_year, df_max_year + 1)]
-                                   ]
-    print(f"[TRANSFORM INFO] Filtering {country_code} WDI data to only include data between {df_min_year} and {df_max_year}")
+        development_data_transformed = development_data_transformed.loc[:,
+                                    ['Country Code', 'Indicator Name'] +
+                                    [str(year) for year in range(df_min_year, df_max_year + 1)]
+                                    ]
+        print(f"[TRANSFORM INFO] Filtering {country_code} WDI data to only include data between {df_min_year} and {df_max_year}")
 
     # Eliminate Development Indicators where more than half of the years have no data
     # There has to be a more elegant way of doing this. I don't love the create-and-delete column process.
     # Also, "more than half of the years" doesn't feel sensitive enough, but we can come back to this -- ALEX
-    num_year_columns = development_data_transformed.iloc[:, 2:].shape[1]
-    nc_half = num_year_columns // 2
+        num_year_columns = development_data_transformed.iloc[:, 2:].shape[1]
+        nc_half = num_year_columns // 2
 
-    development_data_transformed['NaN_count'] = development_data_transformed.iloc[:, 2:].isna().sum(axis=1)
-    development_data_transformed = development_data_transformed[
-        development_data_transformed['NaN_count'] <= nc_half
-        ]
-    development_data_transformed = development_data_transformed.drop(columns=['NaN_count'])
+        development_data_transformed['NaN_count'] = development_data_transformed.iloc[:, 2:].isna().sum(axis=1)
+        development_data_transformed = development_data_transformed[
+            development_data_transformed['NaN_count'] <= nc_half
+            ]
+        development_data_transformed = development_data_transformed.drop(columns=['NaN_count'])
+        print(f'[TRANSFORM INFO] Filtering {country_code} WDI data to only include Indicators with data for 50% or more of selected years')
 
+    # Handles NULL values as needed using
+    # 1. Backward Fill for minimum year in DataFrame ('bfill')
+    # 2. Forward Fill for maximum year in DataFrame ('ffill')
+    # 3. Linear Interpolation for NULL values between two valid data points ('linear')
+        development_data_transformed.iloc[:, 2:] = (
+            development_data_transformed.iloc[:, 2:]
+            .interpolate(axis=1, method='linear')
+            .bfill(axis=1)
+            .ffill(axis=1)
+        )
+        print(f'[TRANSFORM INFO] Handling NULL values for {country_code} WDI data')
+
+    # Makes floats integers for improved visualizations
+        development_data_transformed.iloc[:, 2:] = development_data_transformed.iloc[:, 2:].astype(int)
+        print(f'[TRANSFORM INFO] Converting floats to integers in {country_code} WDI data')
 
     # Save to CSV if specified
-    if save_to_csv:
-        os.makedirs("../data/filtered_ETL_outputs", exist_ok=True)
-        development_data_transformed.to_csv(f"../data/filtered_ETL_outputs/{country_code}_development_data_transformed.csv", index=False)
+        if save_to_csv:
+            os.makedirs("../data/filtered_ETL_outputs", exist_ok=True)
+            development_data_transformed.to_csv(f"../data/filtered_ETL_outputs/{country_code}_development_data_transformed.csv", index=False)
 
-        print(f"[TRANSFORM INFO] Creating CSV for {country_code} development data at /data/filtered_ETL_outputs/{country_code}_development_disaster_data_transformed.csv")
+            print(f"[TRANSFORM INFO] Creating CSV for {country_code} development data at /data/filtered_ETL_outputs/{country_code}_development_disaster_data_transformed.csv")
 
-    return development_data_transformed
+        return development_data_transformed
 
+    except Exception as e:
+        print(f"[TRANSFORM ERROR] Error transforming development data for {country_code}: {e}")
+        raise
 
 def transform_disaster_data(
         # all default figures chosen during Exploratory Data Analysis
         country_code,
         disaster_data,
-        df_min_year=2008,
-        df_max_year=2020,
+        df_min_year=2007,
+        df_max_year=2021,
         min_total_affected=2000000,
         save_to_csv=False
 ):
@@ -82,22 +102,27 @@ def transform_disaster_data(
         2. (Optional) A CSV file with the transformed data (e.g., USA_disaster_data_transformed.csv).
 
     """
-    disaster_data_transformed = disaster_data.copy()
+    try:
+        disaster_data_transformed = disaster_data.copy()
 
-    disaster_data_transformed = disaster_data_transformed[(disaster_data_transformed['Start Year'] >= df_min_year)
+        disaster_data_transformed = disaster_data_transformed[(disaster_data_transformed['Start Year'] >= df_min_year)
                                                           & (disaster_data_transformed['Start Year'] <= df_max_year)]
-    print(f"[TRANSFORM INFO] Filtering {country_code} EM-DAT data to only include disasters between {df_min_year} and {df_max_year}")
+        print(f"[TRANSFORM INFO] Filtering {country_code} EM-DAT data to only include disasters between {df_min_year} and {df_max_year}")
 
-    disaster_data_transformed = disaster_data_transformed[disaster_data_transformed['Total Affected'] >= min_total_affected]
-    print(f"[TRANSFORM INFO] Filtering {country_code} EM-DAT data to only include disasters with {min_total_affected} or more affected")
+        disaster_data_transformed = disaster_data_transformed[disaster_data_transformed['Total Affected'] >= min_total_affected]
+        print(f"[TRANSFORM INFO] Filtering {country_code} EM-DAT data to only include disasters with {min_total_affected} or more affected")
 
     # Save data to CSV if requested
-    if save_to_csv:
-        os.makedirs("../data/filtered_ETL_outputs", exist_ok=True)
-        disaster_data_transformed.to_csv(f"../data/filtered_ETL_outputs/{country_code}_disaster_data_transformed.csv", index=False)
+        if save_to_csv:
+            os.makedirs("../data/filtered_ETL_outputs", exist_ok=True)
+            disaster_data_transformed.to_csv(f"../data/filtered_ETL_outputs/{country_code}_disaster_data_transformed.csv", index=False)
 
-        print(f"[TRANSFORM INFO] Creating CSV for {country_code} disaster data at /data/filtered_ETL_outputs/{country_code}_disaster_data_transformed.csv")
+            print(f"[TRANSFORM INFO] Creating CSV for {country_code} disaster data at /data/filtered_ETL_outputs/{country_code}_disaster_data_transformed.csv")
 
-    return disaster_data_transformed
+        return disaster_data_transformed
+
+    except Exception as e:
+        print(f"[TRANSFORM ERROR] Error transforming disaster data for {country_code}: {e}")
+        raise
 
 
